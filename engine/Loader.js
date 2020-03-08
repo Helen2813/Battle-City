fff88d183729af0a27f65411ae81db1acbe0c2b5
ElenaPenkina <e.abalenkova@gmail.com>
2020-03-08 22:55:07 +0300

    init Loader

--- engine/Loader.js
+++ engine/Loader.js
@@ -3,7 +3,42 @@
 
     
     class Loader {
-        constructor () {}
+        constructor () {
+            this.loadOrder = {
+                images: [],
+                jsons: []
+            }
+            this.resourses = {
+                images: [],
+                jsons: []
+            }
+        }
+
+        addImage (name, src) {
+            this.loadOrder.images.push({name, src})
+        }
+
+        load (callback) {
+            const promises = []
+
+            for (const ImageData of this.loadOrder.images) {
+                const { name, src } = ImageData
+
+                const promise = Loader
+                    .loadImage(src)
+                    .then( image => {
+                        this.resourses.images[name] = image
+
+                        if (this.loadOrder.images.includes(name)) {
+                            const index = this.loadOrder.images.indexOf(name)
+                            this.loadOrder.images.splice(index, 1)
+                        }
+                    })
+                promises.push(promise)
+            }
+
+            Promise.all(promises).then(() => callback)
+        }
 
         static loadImage (src) {
             return new Promise((resolve, reject) => {
@@ -23,3 +58,5 @@
 
     window.GameEngine = window.GameEngine || {}
     window.GameEngine.Loader = Loader
+
+})();
